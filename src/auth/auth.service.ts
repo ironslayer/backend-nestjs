da5fdceed970a9379e0d4f24b1fb03e81d3c34da
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-05-20 19:35:32 -0400

    feat: integrate JWT authentication with user login and token generation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,6 +8,8 @@ import { UpdateAuthDto } from './dto/update-auth.dto';
 import { User } from './entities/user.entity';
 import { Model } from 'mongoose';
 import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload';
 
 @Injectable()
 export class AuthService {
@@ -15,6 +17,8 @@ export class AuthService {
   constructor(
     @InjectModel( User.name ) 
     private userModel: Model<User>,
+
+    private jwtService: JwtService,
   ){}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
@@ -60,7 +64,7 @@ export class AuthService {
 
     return {
       USER: rest,
-      token: 'ABC-123'
+      token: this.getJwtToken({ id: user.id }),
     }
 
   }
@@ -80,5 +84,10 @@ export class AuthService {
 
   remove(id: number) {
     return `This action removes a #${id} auth`;
+  }
+
+  getJwtToken( payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
+    return token;
   }
 }
