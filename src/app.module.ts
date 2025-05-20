46c67f0b2876d4bf3ccab9e62c317d5e574dafa4
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-05-20 16:53:13 -0400

    feat: add authentication module with basic CRUD operations and environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+import { AuthModule } from './auth/auth.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
+
+    AuthModule
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+
+  // constructor(){
+  //   console.log(process.env)
+  // }
+}
